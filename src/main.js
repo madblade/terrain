5344a7d87d8b7ca2f2202b4f4c18c8161c9396fe
madblade <10941563+madblade@users.noreply.github.com>
2020-05-11 13:59:59 +0200

    feat: rasterizing pass 1

--- src/main.js
+++ src/main.js
@@ -30,92 +30,121 @@ let terrainGenerator = new TerrainGenerator(mesher, fieldModifier, eroder);
 let languageGenerator = new LanguageGenerator();
 let nameGiver = new NameGiver(languageGenerator);
 
+function init3D()
+{
+    let country = {params: defaultParams};
+    country.mesh = terrainGenerator.generateCoast(defaultParams);
+    cityPlacer.placeCities(country);
+    country.rivers = cityPlacer.getRivers(country.mesh, 0.01);
+    country.coasts = mesher.contour(country.mesh, 0);
+    country.terr = cityPlacer.getTerritories(country);
+    country.borders = cityPlacer.getBorders(country);
+    console.log(country.rivers);
+
+    let rasterizer = new Rasterizer();
+    let triMesh = //[];
+        rasterizer.computeTriMesh(country.mesh);
+    console.log('Rasterizing.');
+    rasterizer.heightPass(triMesh);
+    console.log(rasterizer.heightBuffer);
+
+    const width = rasterizer.dimension;
+    const height = rasterizer.dimension;
+    let rb = rasterizer.heightBuffer;
+    let buffer = new Uint8ClampedArray(width * height * 4);
+    for (let i = 0; i < height; ++i) for (let j = 0; j < width; ++j)
+    {
+        let s = i * width + j;
+        let stride = s * 4;
+        buffer[stride    ] = rb[s] * 2 >> 0;
+        buffer[stride + 1] = rb[s] * 2 >> 0;
+        buffer[stride + 2] = rb[s] * 2 >> 0;
+        buffer[stride + 3] = 255;
+    }
+    console.log(buffer);
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+    let iData = ctx.createImageData(width, height);
+    iData.data.set(buffer);
+    ctx.putImageData(iData, 0, 0);
+    let image = document.getElementById('gen-image');
+    image.src = canvas.toDataURL();
+
+    let container = document.getElementById('gl');
+    const dpr = window.devicePixelRatio
+    let w = container.clientWidth * dpr;
+    let h = container.clientHeight * dpr;
+    let scene = new Scene();
+    let camera = new PerspectiveCamera(75, w / h, 0.1, 1000);
+    let renderer = new WebGLRenderer({antialias: true});
+    renderer.setSize(w, h);
+    container.appendChild(renderer.domElement);
+
+    // Rivers
+    let rivers = [...country.coasts, ...country.rivers];
+    for (let i = 0; i < rivers.length; ++i) {
+        const r = rivers[i];
+        let m = new LineBasicMaterial({color: 0xff0000});
+        let pts = [];
+        for (let j = 0; j < r.length; ++j) {
+            let p = r[j];
+            pts.push(new Vector3(p[0], p[1], 0.1));
+        }
+        let g = new BufferGeometry().setFromPoints(pts);
+        let l = new Line(g, m);
+        scene.add(l);
+    }
+
+    // HeightMap
+    let geometry = new BufferGeometry();
+    let positions = new Float32Array(triMesh.length * 3 * 3);
+    for (let i = 0; i < triMesh.length; ++i) {
+        let ti = triMesh[i];
+        positions[9 * i]     = ti[0][0];
+        positions[9 * i + 1] = ti[0][1];
+        positions[9 * i + 2] = ti[0][2];
+
+        positions[9 * i + 3] = ti[1][0];
+        positions[9 * i + 4] = ti[1][1];
+        positions[9 * i + 5] = ti[1][2];
+
+        positions[9 * i + 6] = ti[2][0];
+        positions[9 * i + 7] = ti[2][1];
+        positions[9 * i + 8] = ti[2][2];
+    }
+    let positionAttribute = new BufferAttribute(positions, 3);
+    geometry.setAttribute('position', positionAttribute);
+    geometry.computeVertexNormals();
+    let material = new MeshPhongMaterial(
+        {color: 0x00ff00, side: DoubleSide}
+    );
+    let cube = new Mesh(geometry, material);
+    scene.add(cube);
+
+    let p = new Mesh(
+        new PlaneBufferGeometry(10, 10),
+        new MeshBasicMaterial({color: 0x0000ff})
+    );
+    p.position.set(0, 0, 0);
+    scene.add(p);
+
+    let li = new DirectionalLight(0xffffff, 2);
+    li.position.set(1, -1, 2);
+    scene.add(li);
+    camera.position.z = 5;
+    new OrbitControls(camera, container);
+    let animate = function ()
+    {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    };
+    animate();
+}
+
 // init(
 //     mesher, fieldModifier, eroder, cityPlacer,
 //     terrainGenerator, languageGenerator, nameGiver
 // );
-
-let country = { params: defaultParams };
-country.mesh = terrainGenerator.generateCoast(defaultParams);
-cityPlacer.placeCities(country);
-country.rivers = cityPlacer.getRivers(country.mesh, 0.01);
-country.coasts = mesher.contour(country.mesh, 0);
-country.terr = cityPlacer.getTerritories(country);
-country.borders = cityPlacer.getBorders(country);
-console.log(country.rivers);
-
-
-let rasterizer = new Rasterizer();
-let triMesh = rasterizer.computeTriMesh(country.mesh);
-
-let container = document.getElementById('gl');
-const dpr = window.devicePixelRatio
-let w = container.clientWidth * dpr;
-let h = container.clientHeight * dpr;
-let scene = new Scene();
-let camera = new PerspectiveCamera(75, w / h, 0.1, 1000);
-let renderer = new WebGLRenderer({antialias: true});
-renderer.setSize(w, h);
-container.appendChild(renderer.domElement);
-
-// Rivers
-let rivers = [...country.coasts, ...country.rivers];
-for (let i = 0; i < rivers.length; ++i)
-{
-    const r = rivers[i];
-    let m = new LineBasicMaterial({color: 0xff0000});
-    let pts = [];
-    for (let j = 0; j < r.length; ++j)
-    {
-        let p = r[j];
-        pts.push(new Vector3(p[0], p[1], 0.1));
-    }
-    let g = new BufferGeometry().setFromPoints(pts);
-    let l = new Line(g, m);
-    scene.add(l);
-}
-
-// HeightMap
-let geometry = new BufferGeometry();
-let positions = new Float32Array(triMesh.length * 3 * 3);
-for (let i = 0; i < triMesh.length; ++i)
-{
-    let ti = triMesh[i];
-    positions[9 * i]     = ti[0][0];
-    positions[9 * i + 1] = ti[0][1];
-    positions[9 * i + 2] = ti[0][2];
-
-    positions[9 * i + 3] = ti[1][0];
-    positions[9 * i + 4] = ti[1][1];
-    positions[9 * i + 5] = ti[1][2];
-
-    positions[9 * i + 6] = ti[2][0];
-    positions[9 * i + 7] = ti[2][1];
-    positions[9 * i + 8] = ti[2][2];
-}
-let positionAttribute = new BufferAttribute(positions, 3);
-geometry.setAttribute('position', positionAttribute);
-geometry.computeVertexNormals();
-let material = new MeshPhongMaterial(
-    { color: 0x00ff00, side: DoubleSide }
-    );
-let cube = new Mesh(geometry, material);
-scene.add(cube);
-
-let p = new Mesh(
-    new PlaneBufferGeometry(10, 10),
-    new MeshBasicMaterial({ color: 0x0000ff})
-);
-p.position.set(0, 0, 0);
-scene.add(p);
-
-let li = new DirectionalLight(0xffffff, 2);
-li.position.set(1, -1, 2);
-scene.add(li);
-camera.position.z = 5;
-new OrbitControls(camera, container);
-let animate = function () {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-};
-animate();
+init3D();
--- src/terrain/pixel.js
+++ src/terrain/pixel.js
@@ -1,7 +1,11 @@
 
 let Rasterizer = function()
 {
+    this.dimension = 512;
 
+    this.heightBuffer = [];
+    this.chunkBiomes = [];
+    this.surfaceBuffer = [];
 };
 
 Rasterizer.prototype.computeTriMesh = function(
@@ -92,7 +96,7 @@ Rasterizer.prototype.interpolate = function (min, max, gradient)
 // drawing line between 2 points from left to right
 // papb -> pcpd
 // pa, pb, pc, pd must then be sorted before
-Rasterizer.prototype.processScanLine = function (y, pa, pb, pc, pd, color)
+Rasterizer.prototype.processScanLine = function (y, pa, pb, pc, pd)
 {
     // Thanks to current Y, we can compute the gradient to compute others values like
     // the starting X (sx) and ending X (ex) to draw between
@@ -101,16 +105,21 @@ Rasterizer.prototype.processScanLine = function (y, pa, pb, pc, pd, color)
     let gradient2 = pc.y !== pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
 
     let sx = this.interpolate(pa.x, pb.x, gradient1) >> 0;
+    let sh = this.interpolate(pa.z, pb.z, gradient1);
     let ex = this.interpolate(pc.x, pd.x, gradient2) >> 0;
+    let eh = this.interpolate(pc.z, pd.z, gradient2);
 
+    const offset = this.dimension * y;
     // drawing a line from left (sx) to right (ex)
-    for(let x = sx; x < ex; x++) {
+    for (let x = sx; x < ex; ++x)
+    {
+        this.heightBuffer[offset + x] = (sh + (eh - sh) * x / (ex - sx) * 16) >> 0;
         // this.drawPoint(new BABYLON.Vector2(x, y), color);
         // TODO draw
     }
 };
 
-Rasterizer.prototype.drawTriangle = function (p1, p2, p3, color)
+Rasterizer.prototype.drawTriangle = function (p1, p2, p3)
 {
     // Sorting the points in order to always have this order on screen p1, p2 & p3
     // with p1 always up (thus having the Y the lowest possible to be near the top screen)
@@ -135,19 +144,12 @@ Rasterizer.prototype.drawTriangle = function (p1, p2, p3, color)
     // inverse slopes
     let dP1P2; let dP1P3;
 
-    // http://en.wikipedia.org/wiki/Slope
     // Computing slopes
-    if (p2.y - p1.y > 0) {
-        dP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
-    } else {
-        dP1P2 = 0;
-    }
+    if (p2.y - p1.y > 0) dP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
+    else dP1P2 = 0;
 
-    if (p3.y - p1.y > 0) {
-        dP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
-    } else {
-        dP1P3 = 0;
-    }
+    if (p3.y - p1.y > 0) dP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
+    else dP1P3 = 0;
 
     // P1
     // -
@@ -156,11 +158,11 @@ Rasterizer.prototype.drawTriangle = function (p1, p2, p3, color)
     // P3
     if (dP1P2 > dP1P3)
     {
-        for (let y = p1.y >> 0; y <= p3.y >> 0; y++) {
+        for (let y = p1.y >> 0; y <= p3.y >> 0; ++y) {
             if (y < p2.y) {
-                this.processScanLine(y, p1, p3, p1, p2, color);
+                this.processScanLine(y, p1, p3, p1, p2);
             } else {
-                this.processScanLine(y, p1, p3, p2, p3, color);
+                this.processScanLine(y, p1, p3, p2, p3);
             }
         }
     }
@@ -172,36 +174,55 @@ Rasterizer.prototype.drawTriangle = function (p1, p2, p3, color)
     //       P3
     else
     {
-        for (let y = p1.y >> 0; y <= p3.y >> 0; y++) {
+        for (let y = p1.y >> 0; y <= p3.y >> 0; ++y) {
             if (y < p2.y) {
-                this.processScanLine(y, p1, p2, p1, p3, color);
+                this.processScanLine(y, p1, p2, p1, p3);
             } else {
-                this.processScanLine(y, p2, p3, p1, p3, color);
+                this.processScanLine(y, p2, p3, p1, p3);
             }
         }
     }
 };
 
-Rasterizer.prototype.drawTriMesh = function (cMesh)
+Rasterizer.prototype.heightPass = function (triMesh)
 {
-    for (let indexFaces = 0; indexFaces < cMesh.Faces.length; indexFaces++)
+    const width = this.dimension;
+    const height =  this.dimension;
+    this.heightBuffer = new Int32Array(width * height);
+
+    const nbTris = triMesh.length;
+    for (let i = 0; i < nbTris; ++i)
     {
-        let currentFace = cMesh.Faces[indexFaces];
-        let vertexA = cMesh.Vertices[currentFace.A];
-        let vertexB = cMesh.Vertices[currentFace.B];
-        let vertexC = cMesh.Vertices[currentFace.C];
+        let t = triMesh[i];
 
-        let pixelA = this.project(vertexA, transformMatrix);
-        let pixelB = this.project(vertexB, transformMatrix);
-        let pixelC = this.project(vertexC, transformMatrix);
+        let pixelA = { x: (0.5 + t[0][0]) * width, y: (0.5 + t[0][1]) * height, z: t[0][2] };
+        let pixelB = { x: (0.5 + t[1][0]) * width, y: (0.5 + t[1][1]) * height, z: t[1][2] };
+        let pixelC = { x: (0.5 + t[2][0]) * width, y: (0.5 + t[2][1]) * height, z: t[2][2] };
 
-        let color = 0.25 + ((indexFaces % cMesh.Faces.length) / cMesh.Faces.length) * 0.75;
-        this.drawTriangle(pixelA, pixelB, pixelC,
+        // let heights = [t[0][2], t[1][2], t[2][2]];
+            // 0.25 + ((indexFaces % cMesh.Faces.length) / cMesh.Faces.length) * 0.75;
+        this.drawTriangle(pixelA, pixelB, pixelC//,
+            // heights
             // new BABYLON.Color4(color, color, color, 1)
             // TODO wire in height into color
             // and compute normal
         );
     }
+}
+
+Rasterizer.prototype.riverPass = function(mesh)
+{
+
+}
+
+Rasterizer.prototype.cityPass = function(mesh)
+{
+
+}
+
+Rasterizer.prototype.treePass = function(mesh)
+{
+
 }
 
 export { Rasterizer };
